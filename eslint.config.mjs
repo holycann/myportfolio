662647c0989d9335d5c13403a23268586732559c
holycann <muhamad.ramadhan.dev@gmail.com>
2025-06-23 23:57:12 +0700

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigNext from "eslint-config-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,15 +10,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  eslintConfigNext,
   {
     rules: {
       "react/no-unescaped-entities": "off",
       "no-unused-vars": "off",
-      "@next/next/no-img-element": "off"
+      "@next/next/no-img-element": "off",
+      "react-hooks/exhaustive-deps": "off"
     }
   }
 ];
-
-export default eslintConfig;
